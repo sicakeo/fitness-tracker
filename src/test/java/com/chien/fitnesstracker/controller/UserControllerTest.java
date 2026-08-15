1f1279e47f918338721f4fb1d8f313ea7e758919
David <chiendinhbach9@gmail.com>
2026-08-15 08:57:37 -0500

    fix: downgrade java back to version 21 to be sustainable with the project refactor:  add correct dependencies for testing feat: implemennting UserControllerTest

--- src/test/java/com/chien/fitnesstracker/controller/UserControllerTest.java
+++ src/test/java/com/chien/fitnesstracker/controller/UserControllerTest.java
@@ -7,10 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.boot.test.mock.web.*;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
